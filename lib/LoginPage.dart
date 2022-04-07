4854bd3a02abd333c7009fa23b4d4698f8881e51
Ayoub Elhioui <Elhioui001@gmail.com>
2022-04-07 20:31:46 +0000

    signup page done

---
+++ lib/LoginPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(LoginPage());
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/SignupPage.dart
+++ lib/SignupPage.dart
@@ -10,53 +10,83 @@ class SignUp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.amber),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          IconButton(
-            padding: const EdgeInsets.only(right: 170),
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.arrow_back),
+      home: SafeArea(
+
+        child: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [ 
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: IconButton(
+                      padding: const EdgeInsets.only(right: 100),
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            'Create an account, it\'s free',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(labelText: 'Confirm Password'),
+                            obscureText: true,
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          SizedBox(
+                            width: 300,
+                            child: ElevatedButton(
+                              onPressed: () => {},
+                              child: const Text('Sign up'),
+                              style: ElevatedButton.styleFrom(
+                                  shape: const StadiumBorder()),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('Already have an account?'),
+                              TextButton(
+                                onPressed: () => {},
+                                child: const Text('Login'),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 50),
-            child: Column(
-              children: [
-                const Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Create an account, it\'s free',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                const TextField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                ),
-                const TextField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                ),
-                const TextField(
-                  decoration: InputDecoration(labelText: 'Confirm Password'),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 50,),
-                SizedBox(
-                  width: 300,
-                  child: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Text('Sign up'),
-                    style:
-                        ElevatedButton.styleFrom(shape: const StadiumBorder()),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ]),
+        ),
       ),
     );
   }
